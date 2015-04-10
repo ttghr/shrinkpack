c1d88ee0ccc36a32bedc7385a64cd5bded8450ba
Jamie Mason <siunmaru@gmail.com>
2015-04-10 20:01:59 +0100

    refactor(shrinkpack): Organise into a chain of promises

--- index.js
+++ index.js
@@ -2,137 +2,144 @@
 
 'use strict';
 
-var childProcess = require('child_process');
+var PWD = process.env.PWD || process.cwd();
+
 var fs = require('fs');
 var is = require('is');
 var path = require('path');
-var shell = require('shelljs');
+var shell = require('./src/shell');
 var when = require('when');
 
-var log = require('./src/log');
-var tarball = require('./src/tarball');
-var traverse = require('./src/traverse');
-var terminal = require('./src/terminal');
+return shell.getPathToNpmCache()
+  .then(getOptions)
+  .then(clean)
+  .then(prepare)
+  .then(createDependencyGraph)
+  .then(readDependencyGraph)
+  .then(npmCacheDependencies)
+  .then(saveDependencies)
+  .then(updateDependencyGraph)
+  .then(saveDependencyGraph);
 
-function getProjectPath() {
-  return process.env.PWD || process.cwd();
+function getOptions(npmCachePath) {
+  return {
+    dependencies: {
+      graph: null,
+      all: []
+    },
+    paths: {
+      nodeModules: joinAbsolute(PWD, 'node_modules'),
+      nodeShrinkwrap: joinAbsolute(PWD, 'node_shrinkwrap'),
+      npmShrinkwrap: joinAbsolute(PWD, 'npm-shrinkwrap.json'),
+      npmCache: npmCachePath
+    }
+  };
 }
 
-function getFilePath(file) {
-  return path.normalize(
-    path.join(
-      getProjectPath(),
-      file
+function clean(options) {
+  return when.all([
+      shell.deleteDirectory(options.paths.nodeModules),
+      shell.deleteDirectory(options.paths.nodeShrinkwrap),
+      shell.deleteFile(options.paths.npmShrinkwrap)
+    ])
+    .then(function() {
+      return options;
+    });
+}
+
+function prepare(options) {
+  return when.all([
+      shell.createDirectory(options.paths.nodeShrinkwrap),
+      shell.npmInstall()
+    ])
+    .then(function() {
+      return options;
+    });
+}
+
+function createDependencyGraph(options) {
+  return shell.createDependencyGraph()
+    .then(function() {
+      return options;
+    });
+}
+
+function readDependencyGraph(options) {
+  options.dependencies.graph = require(options.paths.npmShrinkwrap);
+  crawlDependencyGraph(options.dependencies.graph, function(name, meta) {
+    options.dependencies.all.push({
+      name: name,
+      meta: meta
+    });
+  });
+  return options;
+}
+
+function npmCacheDependencies(options) {
+  return when.all(
+      options.dependencies.all.map(function(dependency) {
+        return shell.cachePackage(dependency.name, dependency.meta.version);
+      })
     )
+    .then(function() {
+      return options;
+    });
+}
+
+function saveDependencies(options) {
+  return when.all(
+      options.dependencies.all.map(function(dependency) {
+        return shell.copyFile(
+          getPathToNpmCache(options, dependency),
+          getShrinkwrapPath(options, dependency)
+        );
+      })
+    )
+    .then(function() {
+      return options;
+    });
+}
+
+function updateDependencyGraph(options) {
+  options.dependencies.all.forEach(function(dependency) {
+    dependency.meta.resolved = path.relative(PWD, getShrinkwrapPath(options, dependency));
+  });
+  return options;
+}
+
+function saveDependencyGraph(options) {
+  fs.writeFileSync(
+    options.paths.npmShrinkwrap,
+    JSON.stringify(options.dependencies.graph, null, 2)
+  );
+  return options;
+}
+
+function getPathToNpmCache(options, dependency) {
+  return joinAbsolute(
+    options.paths.npmCache, dependency.name, dependency.meta.version, '/package.tgz'
   );
 }
 
-module.exports = {
+function getShrinkwrapPath(options, dependency) {
+  return joinAbsolute(
+    options.paths.nodeShrinkwrap, dependency.name + '-' + dependency.meta.version + '.tgz'
+  );
+}
 
-  update: function(done) {
-    module.exports.clean(function() {
-      module.exports.rebuild(function() {
-        module.exports.save(done);
-      });
+function joinAbsolute() {
+  return path.resolve(
+    path.join.apply(path, [].slice.call(arguments))
+  );
+}
+
+function crawlDependencyGraph(object, fn, key) {
+  if (is.object(object)) {
+    if (is.string(object.resolved)) {
+      fn(key, object);
+    }
+    Object.keys(object).forEach(function(key) {
+      crawlDependencyGraph(object[key], fn, key);
     });
-  },
-
-  clean: function(done) {
-
-    var graph = getFilePath('npm-shrinkwrap.json');
-    var dependencies = getFilePath('node_modules');
-    var savePath = tarball.getSavePath();
-
-    log.info('clean', log.underline(getProjectPath()));
-
-    shell.rm('-f', graph);
-    shell.rm('-rf', dependencies);
-    shell.rm('-rf', savePath);
-
-    done(null);
-
-  },
-
-  rebuild: function(done) {
-
-    var graph = getFilePath('npm-shrinkwrap.json');
-    var dependencies = getFilePath('node_modules');
-
-    log.info('npm install into', log.underline(dependencies));
-
-    console.log(log.styles.gray.open);
-
-    shell.exec('npm install');
-
-    console.log(log.styles.gray.close);
-
-    log.info('npm shrinkwrap into', log.underline(graph));
-
-    shell.exec('npm shrinkwrap --dev', {
-      silent: true
-    });
-
-    done(null);
-
-  },
-
-  save: function(done) {
-
-    log.info('analysing dependency graph');
-
-    var graph = getFilePath('npm-shrinkwrap.json');
-    var graphData = require(graph);
-    var projectPath = getProjectPath();
-    var savePath = tarball.getSavePath();
-    var saveOperations = [];
-
-    log.info('saving dependencies to', log.underline(savePath));
-
-    shell.mkdir('-p', savePath);
-
-    traverse(graphData, function(value, chain) {
-
-      if (is.object(value) && is.string(value.resolved)) {
-
-        var name = chain[chain.length - 1];
-        var version = value.version;
-        var shortName = name + '@' + version;
-        var url = tarball.getRegistryUrl(name, version);
-        var cached = tarball.getCachePath(name, version);
-        var packed = tarball.getSavePath(name, version);
-
-        value.resolved = path.relative(projectPath, packed);
-
-        saveOperations.push(
-          terminal
-          .exec('npm cache add ' + shortName)
-          .then(function() {
-            log.info('save', log.underline(packed));
-            shell.cp('-f', cached, packed);
-          })
-        );
-
-      }
-
-    });
-
-    when.all(saveOperations)
-      .then(
-        function() {
-          log.info('update', log.underline(graph));
-          fs.writeFileSync(graph, JSON.stringify(graphData, null, 2));
-          done(null);
-        },
-        function() {
-          console.log('failure', arguments);
-          done('error');
-        }
-      );
-
   }
-
-};
-
-module.exports.update(function() {
-  log.info('done');
-});
+}
---
+++ src/shell.js
@@ -0,0 +1,77 @@
+'use strict';
+
+var shelljs = require('shelljs');
+var when = require('when');
+var guard = require('when/guard');
+
+var CONCURRENCY_LIMIT = 10;
+var throttle = guard.n(CONCURRENCY_LIMIT);
+
+module.exports = {
+  deleteFile: rateLimit(deleteFile),
+  deleteDirectory: rateLimit(deleteDirectory),
+  createDirectory: rateLimit(createDirectory),
+  copyFile: rateLimit(copyFile),
+  npmInstall: rateLimit(npmInstall),
+  createDependencyGraph: rateLimit(createDependencyGraph),
+  cachePackage: rateLimit(cachePackage),
+  getPathToNpmCache: rateLimit(getPathToNpmCache)
+};
+
+function deleteFile(path) {
+  return when(
+    shelljs.rm('-f', path)
+  );
+}
+
+function deleteDirectory(path) {
+  return when(
+    shelljs.rm('-rf', path)
+  );
+}
+
+function createDirectory(path) {
+  return when(
+    shelljs.mkdir('-p', path)
+  );
+}
+
+function copyFile(from, to) {
+  return when(
+    shelljs.cp('-f', from, to)
+  );
+}
+
+function npmInstall() {
+  return execAsync('npm install');
+}
+
+function createDependencyGraph() {
+  return execAsync('npm shrinkwrap --dev');
+}
+
+function cachePackage(name, version) {
+  return execAsync('npm cache add ' + name + '@' + version);
+}
+
+function getPathToNpmCache() {
+  return execAsync('npm config get cache');
+}
+
+function execAsync(command) {
+  return when.promise(function(resolve, reject) {
+    shelljs.exec(command, {
+      silent: true
+    }, function(code, output) {
+      if (code === 1) {
+        reject(output.trim());
+      } else {
+        resolve(output.trim());
+      }
+    });
+  });
+}
+
+function rateLimit(fn) {
+  return guard(throttle, fn);
+}
